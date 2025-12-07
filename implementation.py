11f6ac48ca5c7528ee8794c2ad4c1855a93f5df9
AntiVeggie <sdummyd2001@gmail.com>
2025-12-07 15:46:37 +0700

    Model Caching, Download Sample, Model Choices

--- implementation.py
+++ implementation.py
@@ -2,22 +2,32 @@ import streamlit as st
 from PIL import Image
 import keras
 from src.utils.predict import predict
-import base64
-import io
 import pandas as pd
-import matplotlib.pyplot as plt
 
-model_type = 'resnet50v2'
-model_path = f'./saved/{model_type}/model.keras'
-model = keras.models.load_model(model_path)
-class_names = ['Glioma', 'Meningioma', 'Sehat', 'Kanker Pituitari']
+class_details = {
+    "Glioma": 
+        "Glioma adalah tumor yang terbentuk ketika sel glia tubuh Anda tumbuh tak terkendali. Sel glia menopang saraf dan membantu sistem saraf pusat bekerja. Jika tumbuh tak terkendali, sel-sel ini dapat membentuk tumor di otak atau sumsum tulang belakang. Glioma adalah tumor primer. Artinya, tumor ini terbentuk langsung di otak atau sumsum tulang belakang.",
 
-details = {
-    "Glioma": "Glioma adalah tumor yang terbentuk ketika sel glia tubuh Anda tumbuh tak terkendali. Sel glia menopang saraf dan membantu sistem saraf pusat bekerja. Jika tumbuh tak terkendali, sel-sel ini dapat membentuk tumor di otak atau sumsum tulang belakang. Glioma adalah tumor primer. Artinya, tumor ini terbentuk langsung di otak atau sumsum tulang belakang.",
-    "Meningioma": "Meningioma adalah tumor yang berkembang dari selaput meninges, yaitu lapisan pelindung otak dan sumsum tulang belakang. Meskipun bukan tumor otak, ia sering dikategorikan demikian karena dapat menekan jaringan otak, saraf, atau pembuluh darah di sekitarnya. Sebagian besar meningioma bersifat jinak (non-kanker) dan tumbuh lambat, tetapi ukuran dan lokasinya bisa menyebabkan gejala yang parah dan mengancam jiwa jika tidak ditangani.",
-    "Sehat": "Tidak ada indikasi otak mengidap tipe kanker otak glioma, meningioma atau pituitari. Hasil ini tidak menyatakan kalau otak sehat sepenuhnya karena itu tetap perlu klarifikasi lebih lanjut dengan dokter yang relevan.",
-    "Kanker Pituitari": "Kanker otak pituitari disebut juga tumor hipofisis atau adenoma pituitari, adalah pertumbuhan sel abnormal di kelenjar pituitari yang menekan area sekitarnya dan mengganggu produksi hormon. Tumor ini sebagian besar bersifat jinak (bukan kanker), namun dapat menyebabkan gejala seperti sakit kepala, gangguan penglihatan, dan masalah hormonal, karena ukurannya yang semakin membesar atau aktivitasnya dalam menghasilkan hormon."
+    "Meningioma": 
+        "Meningioma adalah tumor yang berkembang dari selaput meninges, yaitu lapisan pelindung otak dan sumsum tulang belakang. Meskipun bukan tumor otak, ia sering dikategorikan demikian karena dapat menekan jaringan otak, saraf, atau pembuluh darah di sekitarnya. Sebagian besar meningioma bersifat jinak (non-kanker) dan tumbuh lambat, tetapi ukuran dan lokasinya bisa menyebabkan gejala yang parah dan mengancam jiwa jika tidak ditangani.",
+
+    "Sehat": 
+        "Tidak ada indikasi otak mengidap tipe kanker otak glioma, meningioma atau pituitari. Hasil ini tidak sepenuhnya menyatakan kalau otak sehat, karena masih ada probabilitas otak mengidap tipe kanker otak lain yang tidak termasuk dalam tipe yang dilatih dan ada probabilitas mis-klasifikasi. Diharapkan untuk klarifikasi lebih lanjut dengan dokter yang relevan.",
+
+    "Kanker Pituitari": 
+        "Kanker otak pituitari disebut juga tumor hipofisis atau adenoma pituitari, adalah pertumbuhan sel abnormal di kelenjar pituitari yang menekan area sekitarnya dan mengganggu produksi hormon. Tumor ini sebagian besar bersifat jinak (bukan kanker), namun dapat menyebabkan gejala seperti sakit kepala, gangguan penglihatan, dan masalah hormonal, karena ukurannya yang semakin membesar atau aktivitasnya dalam menghasilkan hormon."
 }
+
+model_types = {
+    'resnet50v2': './saved/resnet50v2/model.keras',
+    'vgg16': './saved/vgg16/model.keras',
+    'mobilenetv2': './saved/mobilenetv2/model.keras'
+}
+
+@st.cache_resource
+def load_model(model_path):
+    model = keras.models.load_model(model_path)
+    return model
 
 st.set_page_config(
     page_title="Brain Tumor Identifier",
@@ -64,14 +74,7 @@ with st.container():
             f"""
             <style>
                 .stButton button {{
-                    width: 160px;
-                    margin-top: 5px;
-                    padding: 12px 20px;
-                    font-size: 18px;
-                    border-radius: 8px;
-                    border: 2px solid #ddd;
                     cursor: pointer;
-                    display: block;
                     background-color: {'#4CAF50' if image else '#808080'};
                     color: white;
                 }}
@@ -87,13 +90,25 @@ with st.container():
             unsafe_allow_html=True
         )
 
-        predict_button = st.button("Predict", disabled=image is None)
+        model_name = st.selectbox("Pilih Model", options=["vgg16", "resnet50v2", "mobilenetv2"], index=0)
+        model = load_model(model_types[model_name])
+
+        col1, col2 = st.columns(2)
+        with col1:
+            predict_button = st.button("Predict", disabled=image is None)
+        with col2:
+            sample_download_button = st.download_button(
+                label="Download Sample",
+                data=open('data/predict_sample.zip', "rb"),
+                file_name="sample_images.zip",
+                mime="application/zip"
+            )
 
     with prob_displayer:
 
         if uploaded_file is not None and predict_button:
 
-            probs = predict(model, image, class_names)
+            probs = predict(model, image, list(class_details.keys()))
             data = pd.DataFrame(list(probs.items()), columns=["Class", "Probability"])
             data["Probability"] = data["Probability"] * 100
             best_class = max(probs, key=probs.get)
@@ -110,20 +125,18 @@ with st.container():
                 ">
                     <h3 style="margin-bottom: 10px;"><b>X-ray Otak diprediksi paling memungkinkan berstatus {best_class}</b></h3>
                     <p style="font-size: 16px;">
-                        {details[best_class]}
+                        {class_details[best_class]}
                     </p>
                 </div>
                 """,
                 unsafe_allow_html=True
             )
 
+            st.subheader("Distribusi Probabilitas setiap tipe kanker otak")
             st.bar_chart(data.set_index("Class")["Probability"], horizontal=True)
-
             cols = st.columns(4)
-        
             for idx, row in data.iterrows():
                 color = '#66cc66' if row['Class'] == best_class else '#ff6666'
-                
                 cols[idx].markdown(
                     f"""
                     <div style='
